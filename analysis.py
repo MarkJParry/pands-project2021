c2444202f217c4ea69c278557f45fea1a765d33b
Mark Parry <G00398271@gmit.ie>
2021-04-26 20:33:48 +0100

    tidying up,adding todays work

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 #Filename:  analysis.py
 #Author:    Mark Parry
 #Created:   23/03/2021
+#Modified:  26/04/2021   by Mark Parry
 #Purpose:   An analysis of the Iris data set
 
 #https://datascience-enthusiast.com/R/pandas_datatable.html  comparison of pandas and R
@@ -8,121 +9,102 @@
 #https://pandas.pydata.org/docs/reference/frame.html
 
 import matplotlib.pyplot as mpl 
-import numpy as np
+#import numpy as np
 import pandas as pd
 
 #read in the csv file of values
 data = pd.read_csv('iris.data',header=None)
 
-
 #give the values a header column as there was no header in the csv file
 data.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+
+#set up an array of mesures only exclude the species column
 measures = data.columns[:-1]
+
 #get the unique values in the species column
 iris_species = data["species"].unique()
-print(iris_species)
+#print(iris_species)
+
 #assign a colour to each species
 colors = {iris_species[0]:"purple",iris_species[1]:"red",iris_species[2]:"blue"}
-#input(colors)
-#input(data)
-#me messing
-#measures = data.columns[:-1]
-#print(measures)
-#input("press any key")
+
+#have a look at the top of the dataframe
 #print(data.head())
+
+#have a look a the bottom
 #print(data.tail())
-with open('describe.txt', 'a') as outfile:
+
+#save the output of the describe function of the dataframe to a text file 
+with open('describe.txt', 'w') as outfile:
      print('\nOutput from the data describe function', file=outfile)
      print(data.describe(),file=outfile)
-#input("pause")
-with open('correlation.txt', 'a') as outfile:
+
+#save the output of the correlation function to a text file
+with open('correlation.txt', 'w') as outfile:
      print('\nOutput from the data correlation function', file=outfile)
      print(data.corr(),file=outfile)
-#
-# input("pause")
 
 
-#mpl.hist(data['sepal_length'],bins=8)
-#mpl.show()
-#input("pause")
 
-#mpl.hist(data[data['species']=='Iris-setosa'][['sepal_length']])
-#a = []
-#a.append(data[data['species']=='Iris-setosa'][['sepal_length']])
-#ris_df[iris_df['species_name'] == 'Setosa']['petal_length']
-a = data[data['species']=='Iris-setosa']['sepal_length']
-#print(a)
-input("pause")
-#print(a.shape)
-
-b = data[data['species']=='Iris-versicolor']['sepal_length']
-#print(b)
-#input("pause")
-c = data[data['species']=='Iris-virginica']['sepal_length']
-mpl_dataset = [a,b,c]
-fig, ax = mpl.subplots(figsize=(12, 7))
-ax.yaxis.set_ticks_position('none')
-# Add major gridlines in the y-axis
-ax.grid(color='grey', axis='y', linestyle='-', linewidth=0.25, alpha=0.5)
-# Set plot title
-ax.set_title('Distribution of sepal_length by species')
-ax.boxplot(mpl_dataset,labels=iris_species)
-
-mpl.show()
+''' not used in this program but useful to get info on data set
 #input("pause")
 #print(type(data))
 #print(list(data))
 #print(data.dtypes)
 #print(data["class"].drop_duplicates())
+'''
 
-
+#define a function to print histograms for the data set
+#this will loop through the measures and within that loop, loop through the species
+#plot the measure for each species on one graph and save that to a file
 def plot_hist():
      #loop through the measurements excluding the species column
      for measure in measures:
-          #print(measure)
-          
           #loop through the dataframe for each species
           for species in iris_species:
-               #print(data[data['class']==iclass][[measure]])
                mpl_data = data[data['species']==species][measure]
                mpl.hist(mpl_data,color=colors[species],label=species,alpha=.5,histtype="step")
-               #Give the histograms a title,change the numbering on the x axis,label both axes, display the legend to note which plot is which(rgb)                    mpl.title("Iris Dataset " + measure + " Histogram") 
-               #mpl.xticks([0,1,2,3,4])
+               #Give the histograms a title,label both axes, display the legend to note which plot is which(rgb)                    
+               mpl.title("Iris Dataset " + measure + " Histogram") 
                mpl.xlabel(measure) 
                mpl.ylabel("count") 
                mpl.legend() 
-               mpl.title("Title")
-               #mpl.savefig(measure + ".png")
+               #this will save the plot to file
+               mpl.savefig(measure + ".png")
           mpl.show()
-#boxplot
-#mpl.figure(figsize = (10, 7))
-#mpl_data = data[data.columns[:-1]] whole set
-#mpl.boxplot(mpl_data)
+
+
+#define a function to print hbox plots  for the data set
+#this will loop through the species and within that loop, loop through the measures
+#plot the measure for each species on one graph and save that to a file
 def plot_box():
      #loop through the dataframe for each species
-
-     
      for species in iris_species:
-          print(species)
+          #print(species)
+          #initialise a list to be used when plotting
           mpl_dataset = []
           for measure in measures:
-               print(measure)
+               #print(measure)
+               #initilise an array for each subset
                mpl_data = []
-               #print(data[data['species']==species][[measure]])
-               #input ("pause")
                mpl_data = data[data['species']==species][measure]
+               #add the subset to the plot set
                mpl_dataset.append(mpl_data)
-               #print(mpl_dataset)
-               #input ("pause")
                #mpl.xlabel(species) 
                mpl.ylabel("count") 
-               #mpl.boxplot(mpl_dataset,labels=data.columns[:-1])
           mpl.title("Boxplots for "+ species + " measures")
-          mpl.boxplot(mpl_dataset,labels=data.columns[:-1])
+          mpl.boxplot(mpl_dataset,labels=measures)
+          #save the plots to file
           mpl.savefig(species + ".png")
+          #display the plots
           mpl.show()
 
+
+#define a function to print scatter plots for the data set
+#this is called by a routine that passes in the combination of measures to be plotted(x,y)
+#plot the measure for each species on one graph and save that to a file
 def plot_scatter(measure1,measure2):
+     #loop throuh the species
      for species in iris_species:
 
           x = data[data['species']==species][measure1]
@@ -134,36 +116,21 @@ def plot_scatter(measure1,measure2):
           mpl.ylabel(measure2)
           mpl.title('Iris Dataset (' + measure1 + " vs  " + measure2 + ")")
           mpl.legend()
+     #save the plots to disk
      mpl.savefig(measure1 + "_" + measure2 + ".png")
+     #display the plots
      mpl.show()
-'''
-fig, axs = plt.subplots(2, 3)
-axs[0, 0].plot(x, y)
-axs[0, 0].set_title('Axis [0, 0]')
-axs[0, 1].plot(x, y, 'tab:orange')
-axs[0, 1].set_title('Axis [0, 1]')
-axs[1, 0].plot(x, -y, 'tab:green')
-axs[1, 0].set_title('Axis [1, 0]')
-axs[1, 1].plot(x, -y, 'tab:red')
-axs[1, 1].set_title('Axis [1, 1]')
-'''
+
+
+##main
+#call the histrogram function
+plot_hist()
 
 #call function to plot scatterplots for possible unique combinations of two measures(4 measures pick two = 4*3/2*1 = 6)
 for i in range (0,4):
      for j in range(i+1,4):
           plot_scatter(measures[i],measures[j])
 
-
-
-             
-#plot_hist()
-#add scatterdiagrams for each pair ([sepal_length,width][petal length,width] for each class (6 diagrams in all)
+#call the box plot function
 plot_box()
 
-#print(data['sepal_length'])
-#setosa_sl = data.loc[0:49, ['sepal_length']]
-#print(data.query('class =="Iris-setosa"'))  #returning error
-#print(data[data['class']=="Iris-setosa"])
-#print(data[data['class']=="Iris-setosa"][['sepal_length']])
-#mpl.hist(setosa_sl)
-#mpl.show()
